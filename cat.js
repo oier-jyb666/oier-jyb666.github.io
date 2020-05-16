b8cebd442c459f744e3010afbbe6eea880e07240
oier-jyb666 <63885754+oier-jyb666@users.noreply.github.com>
2020-05-16 20:48:57 +0800

    Create cat.js

---
+++ cat.js
@@ -0,0 +1,68 @@
+    function getQueryVariable(variable)
+    {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == variable){return pair[1];}
+        }
+        return '';
+    }
+
+    var webSocket = null;
+
+    function onMessage(event) {
+        document.getElementById('messages').innerHTML
+            +=marked("---");
+        var txt=event.data;
+        document.getElementById('messages').innerHTML
+            += txt;
+    }
+
+    function onOpen(event) {
+        document.getElementById('messages').innerHTML
+            = '链接成功';
+    }
+
+    function onError(event) {
+        alert("发生错误");
+    }
+
+    function onClose(event) {
+        alert("连接关闭");
+    }
+
+    function connect() {
+        var sid = getQueryVariable("id");
+        var nickname = getQueryVariable("name");
+        if (url == '' || nickname == '') {
+            alert("房间号和用户名不能为空");
+            return;
+        }
+        var url = 'ws://www.javashitang.com:8090/groupChat/' + sid + '/' + nickname;
+        webSocket = new WebSocket(url);
+        webSocket.onerror = function(event) {
+            onError(event)
+        };
+
+        webSocket.onopen = function(event) {
+            onOpen(event)
+        };
+
+        webSocket.onmessage = function(event) {
+            onMessage(event)
+        };
+
+        webSocket.onclose = function(event) {
+            onClose(event)
+        };
+    }
+
+    function start() {
+        var text = document.getElementById('content').value;
+        text = "<br />" + marked(text);
+        webSocket.send(text);
+        document.getElementById('content').value = '';
+    }
+
+    connect();
